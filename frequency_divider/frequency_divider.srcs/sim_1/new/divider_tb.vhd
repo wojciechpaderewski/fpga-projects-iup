4cb9d0616fb332bf7722466eb7c590b044f65322
mysteriousCzrsX <cezartusken47@gmail.com>
2023-03-13 16:17:50 +0100

    this was suprisingly easy lol

--- frequency_divider/frequency_divider.srcs/sim_1/new/divider_tb.vhd
+++ frequency_divider/frequency_divider.srcs/sim_1/new/divider_tb.vhd
@@ -17,26 +17,40 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
+--Cezary Wieczorkowski
+--Wojciech Paderewski
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity divider_tb is
 end divider_tb;
 
 architecture Behavioral of divider_tb is
-
+    component top is
+        Port(clk_i : in STD_LOGIC;
+           rst_i : in STD_LOGIC;
+           led_o : out STD_LOGIC);
+    end component;
+    
+    constant PERIOD : time := 10ns;     --coresponds to 100Mhz freq
+    
+    signal clk_i : std_logic := '0';
+    signal rst_i : std_logic := '1';    --start with device in reset
+    signal led_o : std_logic;
 begin
-
-
+    UUT : top port map(clk_i => clk_i, rst_i => rst_i, led_o => led_o);
+    
+    clk_i <= not clk_i after PERIOD/2;   --generate simple clock
+    
+    tb : process
+        begin
+            wait for 5ns;
+            rst_i <= '0';
+            wait for 200ns;
+            rst_i <= '1';
+            wait for 5ns;
+            rst_i <= '0';
+            wait;  
+    end process tb;
 end Behavioral;
--- frequency_divider/frequency_divider.srcs/sources_1/new/top.vhd
+++ frequency_divider/frequency_divider.srcs/sources_1/new/top.vhd
@@ -17,29 +17,37 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
+--Cezary Wieczorkowski
+--Wojciech Paderewski
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity top is
-    Port ( clk_i : in STD_LOGIC;
-           rst_i : in STD_LOGIC;
-           led_o : out STD_LOGIC);
+    Port (clk_i : in STD_LOGIC;
+          rst_i : in STD_LOGIC;
+          led_o : out STD_LOGIC);
 end top;
 
 architecture Behavioral of top is
-
+    constant N : natural := 10;
+    signal counter : natural := 0;
+    signal div_out : std_logic := '0';
 begin
-
-
+    division : process(clk_i, rst_i)
+        begin
+            if rst_i = '1' then     --perform reset
+                div_out <= '0';
+                counter <= 0;
+            else 
+                if rising_edge(clk_i) then
+                    counter <= counter + 1;     --increment counter every clock cycle
+                    if counter >= N then        --until divisor is reached
+                        counter <= 0;           --then zero the counter
+                        div_out <= not div_out; -- and change output state
+                    end if;
+                end if;
+            end if;
+    end process division;
+    led_o <= div_out;
 end Behavioral;
