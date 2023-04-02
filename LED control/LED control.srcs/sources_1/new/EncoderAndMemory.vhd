9fe55ac868a2fd8aef99feeff8734455389b7bf6
Wojciech <wojtpaderewski@gmail.com>
2023-04-03 00:11:26 +0200

    Encoder and Memory component

--- LED control/LED control.srcs/sources_1/new/EncoderAndMemory.vhd
+++ LED control/LED control.srcs/sources_1/new/EncoderAndMemory.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -41,8 +42,55 @@ entity EncoderAndMemory is
 end EncoderAndMemory;
 
 architecture Behavioral of EncoderAndMemory is
-
+    signal current_digit_i: STD_LOGIC_VECTOR (31 downto 0) := "00000000000000000000000000000000";
+    signal current_7_segment_number: STD_LOGIC_VECTOR (6 downto 0) := "0000000";
+    type char_tab is array (0 to 15) of std_logic_vector (6 downto 0); --konwersja z binarnej na 7seg
+    constant chars : char_tab := -- a b c d e f g
+                  ("0000001",--0
+                    "1001111",--1
+                    "0010010",--2
+                    "0000110",--3
+                    "1001100",--4
+                    "0100100",--5
+                    "0100000",--6
+                    "0001111",--7
+                    "0000000",--8
+                    "0000100",--9
+                    "0001000",--A
+                    "1100000",--b
+                    "0110001",--C
+                    "1000010",--d
+                    "0110000",--E
+                    "0111000");--F
 begin
-
-
+    SetNumber : process(clk_i, sw_i)
+    begin
+        if rising_edge(clk_i) then
+            current_7_segment_number <= chars(to_integer(unsigned(sw_i(3 downto 0))));
+        end if;
+    end process;
+    ChosseDisplay : process(clk_i, btn_i)
+    begin
+         if rising_edge(clk_i) then
+            case btn_i is
+                when "0001" =>
+                    current_digit_i(7 downto 0) <= current_7_segment_number;
+                when "0010" =>
+                    current_digit_i(14 downto 8) <= current_7_segment_number;
+                when "0100" =>
+                    current_digit_i(22 downto 16) <= current_7_segment_number;
+                when "1000" =>
+                    current_digit_i(30 downto 24) <= current_7_segment_number;
+            end case;
+        end if;
+    end process;
+    SetDots: process(clk_i, sw_i)
+    begin
+        if rising_edge(clk_i) then
+           current_digit_i(7) <= sw_i(4);
+           current_digit_i(15) <= sw_i(5);
+           current_digit_i(23) <= sw_i(6);
+           current_digit_i(31) <= sw_i(7);
+        end if;
+    end process;
 end Behavioral;
