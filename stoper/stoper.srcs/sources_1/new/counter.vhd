e8a90c163ba29dec72d4a347e00fff0722ce0267
mysteriousCzrsX <cezartusken47@gmail.com>
2023-04-17 12:21:36 +0200

    added state machine

--- stoper/stoper.srcs/sources_1/new/counter.vhd
+++ stoper/stoper.srcs/sources_1/new/counter.vhd
@@ -6,7 +6,8 @@ entity counter is
     Port(
         clk_i : in std_logic;
         rst_i : in std_logic;
-        count_i : out std_logic_vector(15 downto 0)
+        inhibit_i : in std_logic;
+        count : out std_logic_vector(15 downto 0)
     );
 end counter;
 
@@ -22,24 +23,30 @@ architecture Behavioral of counter is
     end component;
 
     signal divided_clk : std_logic;
-    signal mut_count_i : out std_logic_vector(15 downto 0)
+    signal inhibited_clk : std_logic; 
+    signal mut_count : std_logic_vector(15 downto 0);
 begin
 
-    d : FrequencyDivider
+    divider : FrequencyDivider
     Generic Map(
         divider => 1000000
     )
     Port Map( 
-        clk_i => clk_i,
+        clk_i => inhibited_clk,
         clk_out => divided_clk 
     );
     counting : process(divided_clk, rst_i)
         begin
             if(rst_i = '1') then
-                mut_count_i <= (others => '0');
+                mut_count <= (others => '0');
             else
                 if rising_edge(divided_clk) then
-                    mut_count_i <= mut_count_i + 1;
+                    mut_count <= mut_count + 1;
                 end if;
             end if;
+    end process counting;
+    
+    count <= mut_count;
+    
+    inhibited_clk <= '0' when inhibit_i = '1' else clk_i;
 end Behavioral;
--- stoper/stoper.srcs/sources_1/new/top.vhd
+++ stoper/stoper.srcs/sources_1/new/top.vhd
@@ -32,7 +32,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity top is
- Port( 
+ port( 
      clk_i : in STD_LOGIC;
      rst_i : in STD_LOGIC;
      start_stop_button_i : in STD_LOGIC;
@@ -44,31 +44,121 @@ end top;
 
 architecture Behavioral of top is
     component debouncer
-        Port(
+        port(
             clk_i : in std_logic;
             input_i : in std_logic;
             output_i : out std_logic
             );
     end component debouncer;
     
+    component counter is
+        port(
+            clk_i : in std_logic;
+            rst_i : in std_logic;
+            inhibit_i : in std_logic;
+            count : out std_logic_vector(15 downto 0)
+            );
+    end component counter;
+    
     type stopwatch_state is (WAITING, COUNTING, STOPPED, OVERFLOW);
+    
+    constant ovf_symbol : std_logic_vector (15 downto 0) := "0010001010111000";
+    
     signal state : stopwatch_state := WAITING; 
+    
     signal rst_i_deb : std_logic; 
-    signal start_stop_button_i_deb : std_logic; 
+    signal start_stop_button_i_deb : std_logic;
+    
+    signal counter_rst : std_logic := '0';
+    signal counter_inhibit : std_logic := '0';
+    signal counter_ovf_flag : std_logic := '0';
+    signal counter_output : std_logic_vector (15 downto 0);
+    signal counter_output_buffer : std_logic_vector (15 downto 0);
+    
+    
 begin
     
     r_debouncer : debouncer
-    Port map(
+    port map(
         clk_i => clk_i,
         input_i => rst_i,
         output_i => rst_i_deb
     );
 
     s_debouncer : debouncer
-    Port map(
+    port map(
         clk_i => clk_i,
         input_i => start_stop_button_i,
         output_i => start_stop_button_i_deb
-    ); 
+    );
+    
+    c_counter : counter
+    port map(
+        clk_i => clk_i,
+        rst_i => counter_rst,
+        inhibit_i => counter_inhibit,
+        count => counter_output
+    );      
+    
+    stopwatch : process(clk_i, rst_i_deb)
+    variable btn_flag : integer := 0;
+    variable btn_pressed : integer := 0;
+        begin
+            if (rst_i_deb = '1') then
+                counter_rst <= '1';
+                state <= WAITING;
+                --possibly add sth
+            elsif rising_edge (clk_i) then
             
+                if btn_flag = 0 and start_stop_button_i_deb = '1' then
+                    btn_flag := 1;
+                elsif btn_flag = 1 and start_stop_button_i_deb = '0' then
+                    btn_pressed := 1;
+                    btn_flag := 0;
+                end if;
+                
+                case state is
+                
+                    when WAITING =>
+                        counter_rst <= '1';
+                        counter_ovf_flag <= '0';
+                        
+                        if btn_pressed = 1 then
+                            state <= COUNTING;
+                        end if;
+                        
+                    when COUNTING => 
+                        counter_rst <= '0';
+                        
+                        if btn_pressed = 1 then
+                            state <= STOPPED;
+                        end if;
+                        
+                    when STOPPED =>
+                        counter_inhibit <= '1';
+                        
+                        if btn_pressed = 1 then
+                            state <= WAITING;
+                        end if;
+                        
+                    when OVERFLOW =>
+                        counter_ovf_flag <= '1';
+                        
+                        if btn_pressed = 1 then
+                            state <= WAITING;
+                        end if;
+                        
+                    when others =>
+                        state <= WAITING;
+                        
+                end case;
+                
+                btn_pressed := 0;
+                
+            end if;
+            
+    end process stopwatch;
+    
+    counter_output_buffer <= ovf_symbol when counter_ovf_flag = '1' else counter_output;
+    
 end Behavioral;
