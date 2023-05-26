5b0fec810f63f9e99c2b6d9ba6ed163c2d03a39e
Wojciech <wojtpaderewski@gmail.com>
2023-05-26 11:13:42 +0200

    top compnents declaration

--- ps2_interface/ps2_interface.srcs/sources_1/new/top.vhd
+++ ps2_interface/ps2_interface.srcs/sources_1/new/top.vhd
@@ -33,8 +33,41 @@ entity top is
 end top;
 
 architecture Behavioral of top is
+    component Seg_Display is
+        port( 
+            clk_i : in STD_LOGIC;
+            digit_i : in STD_LOGIC_VECTOR (6 downto 0);
+            led7_an_o : out STD_LOGIC_VECTOR (3 downto 0);
+            led7_seg_o : out STD_LOGIC_VECTOR (7 downto 0)
+            );
+    end component Seg_Display;
+    component Encoder is
+        port(
+            clk_i : in STD_LOGIC;
+            rst_i : in std_logic;
+            digit_in : in STD_LOGIC_VECTOR (7 downto 0);
+            digit_out : out STD_LOGIC_VECTOR (6 downto 0)
+            );
+    end component Encoder;
+    
+    signal enc_dis_digit_bridge : std_logic_vector (6 downto 0);
+    signal ps2_enc_digit_bridge : std_logic_vector (7 downto 0);
 
 begin
-
+    digit_decoder : Encoder
+    port map(
+        clk_i => clk_i,
+        rst_i => rst_i,
+        digit_in => ps2_enc_digit_bridge,
+        digit_out => enc_dis_digit_bridge
+    );    
+    
+    displayer : Seg_Display
+    port map(
+        clk_i => clk_i,
+        digit_i => enc_dis_digit_bridge,
+        led7_an_o => led7_an_o,
+        led7_seg_o => led7_seg_o
+    );
 
 end Behavioral;
